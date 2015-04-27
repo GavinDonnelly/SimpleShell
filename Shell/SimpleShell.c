3740e8ea2be4477bd61f2727e9edc1f643b1d0ed
Gavin Donnelly <Gavin@gavindonnelly.com>
2015-04-27 17:32:49 +0100

    Added Shell Folder.
    Added Shell folder, to store all c file that will run the shell code.
    Currently SimpleShell.c is just the initial comment.

---
+++ Shell/SimpleShell.c
@@ -0,0 +1,15 @@
+/***************************************************************************
+ * File: SimpleShell.c
+ *
+ * Synopsis: Simple Shell built in c, to the spec: 
+ *			 http://gavindonnelly.com/code-challenge/2015/4/21/simple-shell
+ *
+ * Authors:
+ *      Gavin Donnelly
+ *
+ * Email:
+ *		Gavin@GavinDonnelly.com
+ *
+ * Repo:
+ *		https://github.com/GavinDonnelly/SimpleShell
+ *********************************************************************/
